160eae1ef58438518ca9b7c3ac6dc7d7dc708b7e
LucasRueenaufer <lucas@rueenaufer.de>
2026-04-17 16:32:53 +0200

    Added small description to main

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 17 16:27:10 2026
+
+@author: LucasR
+
+This program aims to solve the heat equation by variational finite element 
+methods on complex geometry domains with boundary conditions. The goal is
+to see how my cast iron pan heats up my lunch (tofu).
+"""
